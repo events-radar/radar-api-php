c75766de74cdf1519a1152520a174d95e901f03e
ekes <ekes@iskra.net>
2016-10-29 13:23:06 +0200

    Change domain for tests from new-radar to radar.

--- tests/Entity/EventTest.php
+++ tests/Entity/EventTest.php
@@ -17,22 +17,22 @@ class EventTest extends EntityTestCase {
     $this->assertEquals($event->getInternalId(), '9171');
     $this->assertEquals($event->getInternalVid(), '9680');
     // Node level fields
-    $this->assertEquals($event->apiUri(), 'https://new-radar.squat.net/api/1.0/node/69300100-b104-4c37-b651-48351543e8a6');
+    $this->assertEquals($event->apiUri(), 'https://radar.squat.net/api/1.0/node/69300100-b104-4c37-b651-48351543e8a6');
     $body_text = "<p>This is a handy event that site devs are using.</p>\n";
     $this->assertEquals($event->getBody(), $body_text);
     $this->assertEquals($event->getBodyRaw(), array('value' => $body_text, 'summary' => '', 'format' => 'rich_text_editor'));
-    $this->assertEquals($event->getUrlView(),'https://new-radar.squat.net/en/event/amsterdam/joes-garage/2014-02-24/test-event');
-    $this->assertEquals($event->getUrlEdit(),'https://new-radar.squat.net/en/node/9171/edit');
+    $this->assertEquals($event->getUrlView(),'https://radar.squat.net/en/event/amsterdam/joes-garage/2014-02-24/test-event');
+    $this->assertEquals($event->getUrlEdit(),'https://radar.squat.net/en/node/9171/edit');
     $this->assertEquals($event->getStatus(), TRUE);
     $this->assertEquals($event->getCreated()->getTimestamp(),'1424807163');
     $this->assertEquals($event->getUpdated()->getTimestamp(),'1424807163');
     // Node level references
     $categories = $event->getCategories();
     $this->assertTrue($categories[0] instanceof \Radar\Connect\Entity\TaxonomyTerm);
-    $this->assertEquals($categories[0]->apiUri(),'https://new-radar.squat.net/api/1.0/taxonomy_term/e85a688d-03ac-4008-a3cb-1adb7e8f718a');
+    $this->assertEquals($categories[0]->apiUri(),'https://radar.squat.net/api/1.0/taxonomy_term/e85a688d-03ac-4008-a3cb-1adb7e8f718a');
     $topics = $event->getTopics();
     $this->assertTrue($topics[0] instanceof \Radar\Connect\Entity\TaxonomyTerm);
-    $this->assertEquals($topics[0]->apiUri(), 'https://new-radar.squat.net/api/1.0/taxonomy_term/6c73cff2-9dc9-41db-a79e-f54bf4c010f7');
+    $this->assertEquals($topics[0]->apiUri(), 'https://radar.squat.net/api/1.0/taxonomy_term/6c73cff2-9dc9-41db-a79e-f54bf4c010f7');
     // Simple fields.
     $this->assertTrue($event instanceof Event);
     $this->assertEquals($event->getTitle(), 'Test event');
@@ -45,11 +45,11 @@ class EventTest extends EntityTestCase {
     // Entity references.
     $price = $event->getPriceCategory();
     $this->assertTrue($price[0] instanceof \Radar\Connect\Entity\TaxonomyTerm);
-    $this->assertEquals($price[0]->apiUri(), 'https://new-radar.squat.net/api/1.0/taxonomy_term/9d943d0c-e2bf-408e-9110-4bfb044f60c0');
-    $this->assertEquals($price[1]->apiUri(), 'https://new-radar.squat.net/api/1.0/taxonomy_term/6f4101f4-cd9b-49f2-91a3-203d2b47a3ed');
+    $this->assertEquals($price[0]->apiUri(), 'https://radar.squat.net/api/1.0/taxonomy_term/9d943d0c-e2bf-408e-9110-4bfb044f60c0');
+    $this->assertEquals($price[1]->apiUri(), 'https://radar.squat.net/api/1.0/taxonomy_term/6f4101f4-cd9b-49f2-91a3-203d2b47a3ed');
     $groups = $event->getGroups();
     $this->assertTrue($groups[0] instanceof \Radar\Connect\Entity\Group);
-    $this->assertEquals($groups[0]->apiUri(), 'https://new-radar.squat.net/api/1.0/node/0df4bcd7-54b4-4559-a960-60b5042d3d48');
+    $this->assertEquals($groups[0]->apiUri(), 'https://radar.squat.net/api/1.0/node/0df4bcd7-54b4-4559-a960-60b5042d3d48');
     $raw_dates = $event->getDatesRaw();
     $this->assertEquals($raw_dates[0]['value'], '1393271100');
     $this->assertEquals($raw_dates[0]['time_end'], '2014-02-24T21:00:00+01:00');
@@ -59,6 +59,6 @@ class EventTest extends EntityTestCase {
     $this->assertEquals($dates[0]['end']->getTimezone()->getName(), '+01:00');
     $locations = $event->getLocations();
     $this->assertTrue($locations[0] instanceof \Radar\Connect\Entity\Location);
-    $this->assertEquals($locations[0]->apiUri(), 'https://new-radar.squat.net/api/1.0/location/3c58abc1-e095-4db5-996d-2a064cebb2d3');
+    $this->assertEquals($locations[0]->apiUri(), 'https://radar.squat.net/api/1.0/location/3c58abc1-e095-4db5-996d-2a064cebb2d3');
   }
 }
--- tests/Entity/GroupTest.php
+++ tests/Entity/GroupTest.php
@@ -17,12 +17,12 @@ class GroupTest extends EntityTestCase {
     $this->assertEquals($group->getInternalId(), '41');
     $this->assertEquals($group->getInternalVid(), '8935');
     // Node level fields
-    $this->assertEquals($group->apiUri(), 'https://new-radar.squat.net/api/1.0/node/0df4bcd7-54b4-4559-a960-60b5042d3d48');
+    $this->assertEquals($group->apiUri(), 'https://radar.squat.net/api/1.0/node/0df4bcd7-54b4-4559-a960-60b5042d3d48');
     $body_text = "<p>Joe's Garage is een ontmoetingsplek voor al dan niet krakers uit de transvaalbuurt en omstreken.</p>\n";
     $this->assertEquals($group->getBody(), $body_text);
     $this->assertEquals($group->getBodyRaw(), array('value' => $body_text, 'summary' => '', 'format' => 'rich_text_editor'));
-    $this->assertEquals($group->getUrlView(),'https://new-radar.squat.net/nl/amsterdam/joes-garage?language=nl');
-    $this->assertEquals($group->getUrlEdit(),'https://new-radar.squat.net/nl/node/41/edit?language=nl');
+    $this->assertEquals($group->getUrlView(),'https://radar.squat.net/nl/amsterdam/joes-garage?language=nl');
+    $this->assertEquals($group->getUrlEdit(),'https://radar.squat.net/nl/node/41/edit?language=nl');
     $this->assertEquals($group->getStatus(), TRUE);
     $this->assertEquals($group->getCreated()->getTimestamp(),'1409775185');
     $this->assertEquals($group->getUpdated()->getTimestamp(),'1424352703');
@@ -30,10 +30,10 @@ class GroupTest extends EntityTestCase {
     $categories = $group->getCategories();
     $this->assertEquals(count($categories), 6);
     $this->assertTrue($categories[0] instanceof \Radar\Connect\Entity\TaxonomyTerm);
-    $this->assertEquals($categories[0]->apiUri(),'https://new-radar.squat.net/api/1.0/taxonomy_term/e97f372b-29bc-460b-bff6-35d2462411ff?language=nl');
+    $this->assertEquals($categories[0]->apiUri(),'https://radar.squat.net/api/1.0/taxonomy_term/e97f372b-29bc-460b-bff6-35d2462411ff?language=nl');
     $topics = $group->getTopics();
     $this->assertTrue($topics[0] instanceof \Radar\Connect\Entity\TaxonomyTerm);
-    $this->assertEquals($topics[0]->apiUri(), 'https://new-radar.squat.net/api/1.0/taxonomy_term/82f00d0a-03df-40ec-a06d-67b875675858?language=nl');
+    $this->assertEquals($topics[0]->apiUri(), 'https://radar.squat.net/api/1.0/taxonomy_term/82f00d0a-03df-40ec-a06d-67b875675858?language=nl');
     // Simple fields.
     $this->assertTrue($group instanceof Group);
     $this->assertEquals($group->getTitle(), 'Joe\'s Garage');
@@ -49,6 +49,6 @@ class GroupTest extends EntityTestCase {
     // Entity references.
     $locations = $group->getLocations();
     $this->assertTrue($locations[0] instanceof \Radar\Connect\Entity\Location);
-    $this->assertEquals($locations[0]->apiUri(), 'https://new-radar.squat.net/api/1.0/location/3c58abc1-e095-4db5-996d-2a064cebb2d3?language=nl');
+    $this->assertEquals($locations[0]->apiUri(), 'https://radar.squat.net/api/1.0/location/3c58abc1-e095-4db5-996d-2a064cebb2d3?language=nl');
   }
 }
